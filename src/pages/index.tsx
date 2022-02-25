b4cf5377ce4fcf964565f3bb34544ed93e67e8a5
shelabh <68127067+shelabh@users.noreply.github.com>
2022-02-26 02:42:53 +0530

    removed footer.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,14 @@
 import type { NextPage } from 'next'
 import Landing from '../components/Landing'
 import Header from '../components/Header'
-import Footer from '../components/Footer'
 
 
 
 const Home: NextPage = () => {
   return (
     <div className="  w-full h-full flex flex-col">
- 
-        <Header />
- 
-    
-        <Landing />
-     
-       
+      <Header />
+      <Landing />
     </div>
   )
 }
